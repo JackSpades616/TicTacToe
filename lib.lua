634ba289fa1158467408474b8b2274d88418e6a5
Jack Spades <jack.spades616@gmail.com>
2017-07-27 09:54:00 +0200

    Bugfix: function isNumeric didn't work.

--- lib.lua
+++ lib.lua
@@ -38,7 +38,7 @@ function Lib:IsNumeric(str)
     local rtn
     if (str) then
         for i = 1, #str do
-            local c = str.split(i,i)
+            local c = str:sub(i,i)
             if (not((c == "0" and i ~= 1) or c == "1" or c == "2" or c == "3" or c == "4" or c == "5" or c == "6" or c == "7" or c == "8" or c == "9")) then
                 return false
             end
